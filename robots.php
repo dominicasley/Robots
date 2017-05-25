8cfc280f5c9c21516aeaf3f9c8bb0d2067847676
Dominic Asley <dasley@dv3.co>
2017-05-25 23:56:41 +1000

    Removes PHPStorm comment template from robots.php

--- robots.php
+++ robots.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dominic
- * Date: 25/05/17
- * Time: 10:13 PM
- */
-
 require __DIR__ . '/Bootstrap/Autoload.php';
 
 $app = new \Robots\Application\RobotApp();
